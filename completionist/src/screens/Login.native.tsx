5e8e2b1aad02f544d9a8b1557db8145486adc6a7
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2023-12-01 19:20:18 +0000

    firebase compiling

--- completionist/src/screens/Login.native.tsx
+++ completionist/src/screens/Login.native.tsx
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { Button, SafeAreaView, ScrollView, StatusBar, Text, TextInput, View, Alert } from 'react-native';
 import StandardLayout from '../components/general/Layouts/StandardLayout.native';
 import QuestList from '../components/custom/QuestList/QuestList.native';
+// import {
+//   GoogleSignin,
+//   GoogleSigninButton,
+//   statusCodes,
+// } from '@react-native-google-signin/google-signin';
 import {
+  GoogleOneTapSignIn,
   GoogleSignin,
   GoogleSigninButton,
   statusCodes,
+  type OneTapUser,
 } from '@react-native-google-signin/google-signin';
 
 interface GoogleSignInError {
@@ -17,34 +24,63 @@ const Login = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState([]);
 
-  const _signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      //accessToken, 
-      const { idToken } = await GoogleSignin.signIn();
+  // const signIn = async () => {
+  //   try {
+  //     await GoogleSignin.hasPlayServices();
+  //     //accessToken, 
+  //     const { idToken } = await GoogleSignin.signIn();
       
-      setLoggedIn(true);
+  //     setLoggedIn(true);
 
-    } catch (error: GoogleSignInError | any) {
+  //   } catch (error: GoogleSignInError | any) {
+  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+  //       // user cancelled the login flow
+  //       Alert.alert('Cancel');
+  //     } else if (error.code === statusCodes.IN_PROGRESS) {
+  //       Alert.alert('Signin in progress');
+  //       // operation (f.e. sign in) is in progress already
+  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+  //       Alert.alert('PLAY_SERVICES_NOT_AVAILABLE');
+  //       // play services not available or outdated
+  //     } else {
+  //       // some other error happened
+  //     }
+  //   }
+  // }
+
+  const signIn = async () => {
+    try {
+      const userInfo = await GoogleOneTapSignIn.signIn({
+        webClientId: config.webClientId,
+        nonce: 'your_nonce',
+      });
+      setState({ userInfo });
+    } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
-        Alert.alert('Cancel');
+        console.log('user cancelled the login flow')
       } else if (error.code === statusCodes.IN_PROGRESS) {
-        Alert.alert('Signin in progress');
-        // operation (f.e. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        Alert.alert('PLAY_SERVICES_NOT_AVAILABLE');
-        // play services not available or outdated
+        // operation (e.g. sign in) is in progress already
+        console.log('operation (e.g. sign in) is in progress already')
+      } else if (error.code === statusCodes.ONE_TAP_START_FAILED) {
+        // starting the one tap dialog failed
+        console.log('starting the one tap dialog failed: ', error.message)
+      } else if (error.code === statusCodes.NO_SAVED_CREDENTIAL_FOUND) {
+        console.log('No saved credentials found.')
+        // No saved credentials found. Launch the One Tap sign-up flow (use GoogleOneTapSignIn.signUp)
+        // or do nothing and continue presenting the signed-out UI.
       } else {
         // some other error happened
+        console.log('some other error happened')
       }
     }
-  }
+  };
   
   const signOut = async () => {
     try {
-      await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
+      // await GoogleSignin.revokeAccess();
+      // await GoogleSignin.signOut();
+      await GoogleOneTapSignIn.signOut(user.id);
       setLoggedIn(false);
       setUser([]);
     } catch (error) {
@@ -73,7 +109,7 @@ const Login = () => {
                 style={{width: 192, height: 48}}
                 size={GoogleSigninButton.Size.Wide}
                 color={GoogleSigninButton.Color.Dark}
-                onPress={_signIn}
+                onPress={signIn}
               />
             </View>
             <View>
