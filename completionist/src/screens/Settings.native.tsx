411d848410375ce08b72dcf6fcc5f6776ba51880
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2023-12-11 10:03:56 +0000

    settings page comments

---
+++ completionist/src/screens/Settings.native.tsx
@@ -0,0 +1,16 @@
+import StandardLayout from "@components/general/Layouts/StandardLayout.native";
+import NavigationHeader from "@navigation/NavigationHeader.native";
+
+const Settings = () => {
+  // Scroll View
+  // Collection Settings
+  // --- Each game should have toggles to switch objectives on or not (i.e. if they don't want to track locations and so disabling this will prevent it from being added to the total completion score.)
+  // TODO: Work out primary and secondary collectable types.
+  return (
+    <StandardLayout>
+      <NavigationHeader title={'Settings'} />
+    </StandardLayout>
+  );
+};
+
+export default Settings;
