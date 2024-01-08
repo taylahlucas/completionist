0b43b0f83ed1bb8ee1c6cae3504b903be1a4d06f
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2024-01-08 12:20:18 +0000

    Removed suspend watering & job sheet

--- completionist/src/screens/Landing.native.tsx
+++ completionist/src/screens/Landing.native.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import StandardLayout from '../components/general/Layouts/StandardLayout.native';
 import StyledText from '@components/general/Text/StyledText.native';
 import useGetTheme from '@styles/hooks/useGetTheme';
-import LandingForm from '@components/custom/LandingForm/LandingForm.native';
+import LottieView from 'lottie-react-native';
 
 const Landing = () => {
   const theme = useGetTheme();
+  const animationRef = useRef<LottieView>(null);
 
   return (
     <StandardLayout>
       <StyledText style={{ marginTop: 32 }} color={theme.lightestGrey}>Completionist.</StyledText>
-      <LandingForm />
+      <LottieView
+        ref={animationRef}
+        style={{ width: 100, height: 100 }}
+        source={require('../../../animations/tick.json')}
+        autoPlay={false}
+        loop={false}
+      />
     </StandardLayout>
   );
 };
