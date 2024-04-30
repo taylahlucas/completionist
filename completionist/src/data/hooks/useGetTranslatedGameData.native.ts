49904c4de9252efa8f1f722b2441caac3de1015a
taylahlucas <taylahlucas.dev@gmail.com>
2024-04-30 12:22:59 +0100

    Updated indexes

--- completionist/src/data/hooks/useGetTranslatedGameData.native.ts
+++ completionist/src/data/hooks/useGetTranslatedGameData.native.ts
@@ -1,17 +1,17 @@
 import * as RNLocalize from 'react-native-localize';
-// import * as ar from '../../../translations/ar/db/index.js';
-// import * as de from '../../../translations/de/db/index.js';
+import * as ar from '../../../translations/ar/db/index.js';
+import * as de from '../../../translations/de/db/index.js';
 import * as en from '../../../translations/en/db/index.js';
-// import * as es from '../../../translations/es/db/index.js';
-// import * as fr from '../../../translations/fr/db/index.js';
-// import * as hi from '../../../translations/hi/db/index.js';
-// import * as id from '../../../translations/id/db/index.js';
-// import * as it from '../../../translations/it/db/index.js';
-// import * as ja from '../../../translations/ja/db/index.js';
-// import * as pt from '../../../translations/pt/db/index.js';
-// import * as tr from '../../../translations/tr/db/index.js';
-// import * as vi from '../../../translations/vi/db/index.js';
-// import * as zh from '../../../translations/zh/db/index.js';
+import * as es from '../../../translations/es/db/index.js';
+import * as fr from '../../../translations/fr/db/index.js';
+import * as hi from '../../../translations/hi/db/index.js';
+import * as id from '../../../translations/id/db/index.js';
+import * as it from '../../../translations/it/db/index.js';
+import * as ja from '../../../translations/ja/db/index.js';
+import * as pt from '../../../translations/pt/db/index.js';
+import * as tr from '../../../translations/tr/db/index.js';
+import * as vi from '../../../translations/vi/db/index.js';
+import * as zh from '../../../translations/zh/db/index.js';
 import useMainState from '@redux/hooks/useMainState';
 
 interface TranslatedGameDataReturnType {
@@ -27,42 +27,42 @@ const useGetTranslatedGameData = (): TranslatedGameDataReturnType => {
 
 	let languageDb = en;
 	switch (userLocale) {
-		// case 'ar':
-		// 	languageDb = ar;
-		// 	break;
-		// case 'de':
-		// 	languageDb = de;
-		// 	break;
-		// case 'es':
-		// 	languageDb = es;
-		// 	break;
-		// case 'fr':
-		// 	languageDb = fr;
-		// 	break;
-		// case 'hi':
-		// 	languageDb = hi;
-		// 	break;
-		// case 'id':
-		// 	languageDb = id;
-		// 	break;
-		// case 'it':
-		// 	languageDb = it;
-		// 	break;
-		// case 'ja':
-		// 	languageDb = ja;
-		// 	break;
-		// case 'pt':
-		// 	languageDb = pt;
-		// 	break;
-		// case 'tr':
-		// 	languageDb = tr;
-		// 	break;
-		// case 'vi':
-		// 	languageDb = vi;
-		// 	break;
-		// case 'zh':
-		// 	languageDb = zh;
-		// 	break;
+		case 'ar':
+			languageDb = ar;
+			break;
+		case 'de':
+			languageDb = de;
+			break;
+		case 'es':
+			languageDb = es;
+			break;
+		case 'fr':
+			languageDb = fr;
+			break;
+		case 'hi':
+			languageDb = hi;
+			break;
+		case 'id':
+			languageDb = id;
+			break;
+		case 'it':
+			languageDb = it;
+			break;
+		case 'ja':
+			languageDb = ja;
+			break;
+		case 'pt':
+			languageDb = pt;
+			break;
+		case 'tr':
+			languageDb = tr;
+			break;
+		case 'vi':
+			languageDb = vi;
+			break;
+		case 'zh':
+			languageDb = zh;
+			break;
 		default:
 			languageDb = en;
 	}
