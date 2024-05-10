6d5dc275bf656bb4f820054d14e2ddb0e9d0141d
taylahlucas <taylahlucas.dev@gmail.com>
2024-05-10 19:26:35 +0100

    fixed list item size

--- completionist/src/components/general/Lists/ListStyledComponents.native.tsx
+++ completionist/src/components/general/Lists/ListStyledComponents.native.tsx
@@ -63,7 +63,7 @@ export const ListItemContainer = styled(Animated.View)`
   align-items: center;
   justify-content: space-between;
   min-height: 50px;
-  width: ${LARGE_WIDTH}px;
+  width: ${LARGE_WIDTH - 16}px;
 `;
 
 export const ListItemContentContainer = styled.View`
