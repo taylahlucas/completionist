ebe10b54cef2f7bb7d29ab6435fa0178fd9592ea
taylahlucas <taylahlucas.dev@gmail.com>
2024-05-14 18:09:43 +0100

    Hide drawer items when data not available

--- completionist/src/navigation/hooks/useGetNavigationDrawerItems.native.ts
+++ completionist/src/navigation/hooks/useGetNavigationDrawerItems.native.ts
@@ -7,43 +7,55 @@ import useCheckSectionEnabled from './useCheckSectionEnabled.native';
 import { NavigationDrawerItemData } from '@utils/CustomInterfaces';
 
 const useGetNavigationDrawerItems = (): NavigationDrawerItemData[] => {
-  const { t } = useTranslation();
-  const { selectedGame } = useMainState();
-  const { getUserQuests, getUserCollectables, getUserLocations, getUserMiscItems } = useGetUserGameData();
-  const { mapDataTo } = useGetGameData();
-  const { checkIsSectionEnabled } = useCheckSectionEnabled();
-  const questsSection = ContentSectionEnum.QUESTS;
-  const collectablesSection = ContentSectionEnum.COLLECTABLES;
-  const locationsSection = ContentSectionEnum.LOCATIONS;
-  const miscItemsSection = ContentSectionEnum.MISCELLANEOUS;
+	const { t } = useTranslation();
+	const { selectedGame } = useMainState();
+	const { getUserQuests, getUserCollectables, getUserLocations, getUserMiscItems } = useGetUserGameData();
+	const { mapDataTo } = useGetGameData();
+	const { checkIsSectionEnabled } = useCheckSectionEnabled();
+	const questsSection = ContentSectionEnum.QUESTS;
+	const collectablesSection = ContentSectionEnum.COLLECTABLES;
+	const locationsSection = ContentSectionEnum.LOCATIONS;
+	const miscItemsSection = ContentSectionEnum.MISCELLANEOUS;
+	const questData = mapDataTo(questsSection, selectedGame, true);
+	const collectableData = mapDataTo(collectablesSection, selectedGame, true);
+	const locationData = mapDataTo(locationsSection, selectedGame, true);
+	const miscellaneousData = mapDataTo(miscItemsSection, selectedGame, true);
 
-	// TODO: Hide sections when there is no data e.g. no misc items for witcher 3
-  return ([
-    {
-      id: AuthScreenEnum.Quests,
-      title: t('common:screens.quests'),
-      subTitle: checkIsSectionEnabled(questsSection) ? `${getUserQuests().length}/${mapDataTo(questsSection, selectedGame, true).length}` : '',
-      isEnabled: checkIsSectionEnabled(questsSection)
-    },
-    {
-      id: AuthScreenEnum.Collectables,
-      title: t('common:screens.collectables'),
-      subTitle: checkIsSectionEnabled(collectablesSection) ? `${getUserCollectables().length}/${mapDataTo(collectablesSection, selectedGame, true).length}` : '',
-      isEnabled: checkIsSectionEnabled(collectablesSection)
-    },
-    {
-      id: AuthScreenEnum.Locations,
-      title: t('common:screens.locations'),
-      subTitle:  checkIsSectionEnabled(locationsSection) ? `${getUserLocations().length}/${mapDataTo(locationsSection, selectedGame, true).length}` : '',
-      isEnabled: checkIsSectionEnabled(locationsSection)
-    },
-    {
-      id: AuthScreenEnum.Miscellaneous,
-      title: t('common:screens.miscellaneous'),
-      subTitle: checkIsSectionEnabled(miscItemsSection) ? `${getUserMiscItems().length}/${mapDataTo(miscItemsSection, selectedGame, true).length}` : '',
-      isEnabled: checkIsSectionEnabled(miscItemsSection)
-    }
-  ]);
+	let drawerItems = [];
+	if (questData.length > 0) {
+		drawerItems.push({
+			id: AuthScreenEnum.Quests,
+			title: t('common:screens.quests'),
+			subTitle: checkIsSectionEnabled(questsSection) ? `${getUserQuests().length}/${questData.length}` : '',
+			isEnabled: checkIsSectionEnabled(questsSection)
+		},)
+	}
+	if (collectableData.length > 0) {
+		drawerItems.push({
+			id: AuthScreenEnum.Collectables,
+			title: t('common:screens.collectables'),
+			subTitle: checkIsSectionEnabled(collectablesSection) ? `${getUserCollectables().length}/${collectableData.length}` : '',
+			isEnabled: checkIsSectionEnabled(collectablesSection)
+		})
+	}
+	if (locationData.length > 0) {
+		drawerItems.push({
+			id: AuthScreenEnum.Locations,
+			title: t('common:screens.locations'),
+			subTitle: checkIsSectionEnabled(locationsSection) ? `${getUserLocations().length}/${locationData.length}` : '',
+			isEnabled: checkIsSectionEnabled(locationsSection)
+		})
+	}
+	if (miscellaneousData.length > 0) {
+		drawerItems.push({
+			id: AuthScreenEnum.Miscellaneous,
+			title: t('common:screens.miscellaneous'),
+			subTitle: checkIsSectionEnabled(miscItemsSection) ? `${getUserMiscItems().length}/${miscellaneousData.length}` : '',
+			isEnabled: checkIsSectionEnabled(miscItemsSection)
+		})
+	}
+	
+	return drawerItems;
 };
 
 export default useGetNavigationDrawerItems;
