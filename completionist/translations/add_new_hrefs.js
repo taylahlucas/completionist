4cf67b75e7f65b3bec9dcc6c7289e37c59e87633
taylahlucas <taylahlucas.dev@gmail.com>
2024-05-21 16:40:54 +0100

    Updated skyrim db with hrefs

---
+++ completionist/translations/add_new_hrefs.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+// Function to add UUID to each object in the array
+const compareHrefs = (objects, updateObject) => {
+	return updateObject.map((obj, index) => ({
+		...obj,
+		href: objects[index].href
+	}));
+};
+
+// Read JSON file
+const readJsonFile = (filePath) => {
+	try {
+		const data = fs.readFileSync(filePath, 'utf8');
+		return JSON.parse(data);
+	} catch (err) {
+		console.error('Error reading JSON file:', err.message);
+		return [];
+	}
+};
+
+// Write JSON file
+const writeJsonFile = (filePath, data) => {
+	try {
+		fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+		console.log('File updated successfully! ', filePath);
+	} catch (err) {
+		console.error('Error writing JSON file:', err.message);
+	}
+};
+
+const languageList = ['ar', 'de', 'es', 'fr', 'hi', 'id', 'it', 'ja', 'pt', 'tr', 'vi', 'zh'];
+
+
+const jsonFilePath = 'en/db/skyrim.json';
+const objects = readJsonFile(jsonFilePath);
+
+languageList.forEach((lang) => {
+	const updateJsonFilePath = `${lang}/db/skyrim.json`;
+
+	// Read objects from the JSON file
+	const objectsToUpdate = readJsonFile(updateJsonFilePath);
+
+	// Add UUID to each object
+	const objectsWithUuid = compareHrefs(objects, objectsToUpdate);
+
+	// Write updated objects back to the JSON file
+	writeJsonFile(updateJsonFilePath, objectsWithUuid);
+});
+
