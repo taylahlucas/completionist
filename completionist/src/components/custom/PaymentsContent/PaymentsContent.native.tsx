b172c9e90df88cf32d143a7988715ede6e77f452
taylahlucas <taylahlucas.dev@gmail.com>
2024-07-11 11:41:17 +0100

    cleanup

--- completionist/src/components/custom/PaymentsContent/PaymentsContent.native.tsx
+++ completionist/src/components/custom/PaymentsContent/PaymentsContent.native.tsx
@@ -8,7 +8,6 @@ import SubscriptionOptionDescription from '../SubscriptionContent/SubscriptionOp
 import PriceItem from '@components/general/PriceItem/PriceItem.native';
 import { PaymentPriceItem, PaymentPricesContainer, PaymentPlanSubtitle } from './PaymentsContentStyledComponents.native';
 import Button from '@components/general/Button/Button.native';
-import useReactNavigation from '@navigation/hooks/useReactNavigation.native';
 import ScrollableList from '@components/general/Lists/ScrollableList.native';
 import useMainState from '@redux/hooks/useMainState';
 import Spacing from '@components/general/Spacing.native';
@@ -18,7 +17,6 @@ const PaymentsContent = () => {
 	const theme = useGetTheme();
 	const { selectedSubscription } = useSubscriptionState();
 	const [selectedPrice, setSelectedPrice] = useState(selectedSubscription.prices[0]);
-	const { user } = useMainState();
 
 	return (
 		<>
