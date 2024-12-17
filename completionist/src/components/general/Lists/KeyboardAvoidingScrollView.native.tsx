1278ea00c634fdfcbd9dcb08f7393af6c12a195b
Taylah Lucas <taylahlucas.dev@gmail.com>
2024-12-17 19:10:15 +0000

    Cleanup

--- completionist/src/components/general/Lists/KeyboardAvoidingScrollView.native.tsx
+++ completionist/src/components/general/Lists/KeyboardAvoidingScrollView.native.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { KeyboardAvoidingView } from 'react-native';
-import {KeyboardAwareContainer} from '@components/general/index';
+import { KeyboardAwareContainer } from '@components/general/index';
 import { ScrollableList } from '@components/general/Lists/index';
 
 interface KeyboardAvoidingScrollViewProps {
-	children: JSX.Element[];
-	awareView: any;
+  children: JSX.Element[];
+  awareView: any;
 }
 
-export const KeyboardAvoidingScrollView = ({ children, awareView }: KeyboardAvoidingScrollViewProps) => {
-	return (
-		<KeyboardAvoidingView 
-			behavior='padding'
-			style={{ flex: 1 }}
-		>
-			<ScrollableList>
-				{children}
-			</ScrollableList>
-			<KeyboardAwareContainer>
-				{awareView}
-			</KeyboardAwareContainer>
-		</KeyboardAvoidingView>
-	);
-};
+export const KeyboardAvoidingScrollView = ({
+  children,
+  awareView,
+}: KeyboardAvoidingScrollViewProps) => (
+  <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+    <ScrollableList>{children}</ScrollableList>
+    <KeyboardAwareContainer>{awareView}</KeyboardAwareContainer>
+  </KeyboardAvoidingView>
+);
